da59b2fd760f267baad3fe6e3209e91104e5de71
yasinrahmatian <yasinrahmatian95@gmail.com>
2023-02-18 09:49:38 +0330

    tes

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', function () {
-    return view('welcome');
-});
